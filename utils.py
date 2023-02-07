c39f01609bd6a389ba544910394ec0eca15b53cc
qbhan <smarthkb8@gmail.com>
2023-02-07 14:24:26 +0000

    Changed saving function based on output dict

--- utils.py
+++ utils.py
@@ -16,16 +16,14 @@ def tonemap(c, ref=None, kInvGamma=1.0/2.2):
     return np.clip(col ** kInvGamma, 0.0, 1.0)
 
 
-def save_aovs(img: np.ndarray, aovs, save_dir='result'):
-    rgb = img[:, :, :3]
-    os.makedirs(save_dir, exist_ok=True)
-    plt.imsave(os.path.join(save_dir, 'radiance.png'), tonemap(rgb))
-    idx = 3
-    for aov in aovs:
-        aov_name, aov_ch = aov.split(':')[0], int(aov.split(':')[1])
-        # print(aov_name)
-        a = img[:, :, idx:idx+aov_ch]
-        idx += aov_ch
+def save_aovs(aov_dict: dict, aovs, save_dir='result'):
+    os.makedirs(save_dir)
+    for k in aov_dict:
+        # print(k, full_dict[k].shape)
+        aov_name = k.split(':')[0]
+        if len(aov_dict[k].shape) == 4: a = aov_dict[k].mean(2)
+        elif len(aov_dict[k].shape) == 3: a = aov_dict[k] 
+        # print(k, a.shape)
         if 'normal' in aov_name:
             plt.imsave(os.path.join(save_dir, '{}.png'.format(aov_name)), np.clip(a * 0.5 + 0.5, 0.0, 1.0))
         elif 'depth' in aov_name:
@@ -34,4 +32,3 @@ def save_aovs(img: np.ndarray, aovs, save_dir='result'):
             plt.imsave(os.path.join(save_dir, '{}.png'.format(aov_name)), np.clip(a, 0.0, 1.0))
         elif 'radiance' in aov_name:
             plt.imsave(os.path.join(save_dir, '{}.png'.format(aov_name)), tonemap(a))
-            plt.imsave(os.path.join(save_dir, 'radiance_spec.png'.format(aov_name)), tonemap(rgb-a))
